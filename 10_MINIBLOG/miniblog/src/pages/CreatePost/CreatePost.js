0e44bf1e7ab79fb2c4884c8b28d0b5ec481ec709
Manoel <manoelluisbusiness@gmail.com>
2022-07-18 00:20:27 -0300

    Validação dos dados

--- 10_MINIBLOG/miniblog/src/pages/CreatePost/CreatePost.js
+++ 10_MINIBLOG/miniblog/src/pages/CreatePost/CreatePost.js
@@ -16,26 +16,40 @@ const CreatePost = () => {
 
   const { insertDocument, response } = useInsertDocument("posts");
 
+  const navigate = useNavigate();
+
   const handleSubmit = (e) => {
     e.preventDefault();
     setFormError("");
 
     //validate image URL
+    try {
+      new URL(image);
+    } catch (error) {
+      setFormError("A imagem precisa ser uma URL");
+    }
 
     //criar array de tags
+    const tagsArray = tags.split(",").map((tag) => tag.trim().toLowerCase());
 
     //checar todos os valores
+    if (!title || !image || !tags || !body) {
+      setFormError("Por favor preencha todos os campos!");
+    }
+
+    if (formError) return;
 
     insertDocument({
       title,
       image,
       body,
-      tags,
+      tagsArray,
       uid: user.uid,
       createdBy: user.displayName,
     });
 
     // redirect to home page
+    navigate("/");
   };
 
   return (
@@ -66,7 +80,7 @@ const CreatePost = () => {
           />
         </label>
         <label>
-          <span>Titulo:</span>
+          <span>Conteudo:</span>
           <textarea
             name="body"
             required
@@ -94,6 +108,7 @@ const CreatePost = () => {
           </button>
         )}
         {response.error && <p className="error">{response.error}</p>}
+        {formError && <p className="error">{formError}</p>}
       </form>
     </div>
   );
