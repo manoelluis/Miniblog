3a1cb8e5703f9dd7766b74d2b640bb3f3817dca7
Manoel <manoelluisbusiness@gmail.com>
2022-07-17 22:06:04 -0300

    Estrutura da pagina de criar post

--- 10_MINIBLOG/miniblog/src/pages/CreatePost/CreatePost.js
+++ 10_MINIBLOG/miniblog/src/pages/CreatePost/CreatePost.js
@@ -11,9 +11,67 @@ const CreatePost = () => {
   const [tags, setTags] = useState([]);
   const [formError, setFormError] = useState("");
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div>
-      <h1>CreatePost</h1>
+    <div className={styles.create_post}>
+      <h2>Criar post</h2>
+      <p>Escreva sobre o que voce quiser e compartilhe seus momentos!</p>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>Titulo:</span>
+          <input
+            type="text"
+            name="title"
+            required
+            placeholder="Pense num bom titulo..."
+            onChange={(e) => setTitle(e.target.value)}
+            value={title}
+          />
+        </label>
+        <label>
+          <span>Url da imagem:</span>
+          <input
+            type="text"
+            name="image"
+            required
+            placeholder="Insira uma imagem que represente seu post..."
+            onChange={(e) => setImage(e.target.value)}
+            value={title}
+          />
+        </label>
+        <label>
+          <span>Titulo:</span>
+          <textarea
+            name="body"
+            required
+            placeholder="Insira o conteudo do post"
+            onChange={(e) => setBody(e.target.value)}
+            value={body}
+          ></textarea>
+        </label>
+        <label>
+          <span>Tags:</span>
+          <input
+            type="text"
+            name="tags"
+            required
+            placeholder="Insira as tags separadas por uma virgula"
+            onChange={(e) => setTags(e.target.value)}
+            value={tags}
+          />
+        </label>
+        <button className="btn">Cadastrar</button>
+        {/* {!loading && <button className="btn">Cadastrar</button>}
+        {loading && (
+          <button className="btn" disabled>
+            Aguarde...
+          </button>
+        )}
+        {error && <p className="error">{error}</p>} */}
+      </form>
     </div>
   );
 };
