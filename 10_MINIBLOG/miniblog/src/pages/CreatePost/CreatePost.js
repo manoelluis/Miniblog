abbc5075b2465d3020c859110aefeb7bdeed4789
Manoel <manoelluisbusiness@gmail.com>
2022-07-17 23:53:43 -0300

    Inserindo dados no Firebase

--- 10_MINIBLOG/miniblog/src/pages/CreatePost/CreatePost.js
+++ 10_MINIBLOG/miniblog/src/pages/CreatePost/CreatePost.js
@@ -3,6 +3,7 @@ import styles from "./CreatePost.module.css";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuthValue } from "../../context/AuthContext";
+import { useInsertDocument } from "../../hooks/useInsertDocument";
 
 const CreatePost = () => {
   const [title, setTitle] = useState("");
@@ -11,8 +12,30 @@ const CreatePost = () => {
   const [tags, setTags] = useState([]);
   const [formError, setFormError] = useState("");
 
+  const { user } = useAuthValue();
+
+  const { insertDocument, response } = useInsertDocument("posts");
+
   const handleSubmit = (e) => {
     e.preventDefault();
+    setFormError("");
+
+    //validate image URL
+
+    //criar array de tags
+
+    //checar todos os valores
+
+    insertDocument({
+      title,
+      image,
+      body,
+      tags,
+      uid: user.uid,
+      createdBy: user.displayName,
+    });
+
+    // redirect to home page
   };
 
   return (
@@ -39,7 +62,7 @@ const CreatePost = () => {
             required
             placeholder="Insira uma imagem que represente seu post..."
             onChange={(e) => setImage(e.target.value)}
-            value={title}
+            value={image}
           />
         </label>
         <label>
@@ -63,14 +86,14 @@ const CreatePost = () => {
             value={tags}
           />
         </label>
-        <button className="btn">Cadastrar</button>
-        {/* {!loading && <button className="btn">Cadastrar</button>}
-        {loading && (
+
+        {!response.loading && <button className="btn">Cadastrar</button>}
+        {response.loading && (
           <button className="btn" disabled>
             Aguarde...
           </button>
         )}
-        {error && <p className="error">{error}</p>} */}
+        {response.error && <p className="error">{response.error}</p>}
       </form>
     </div>
   );
