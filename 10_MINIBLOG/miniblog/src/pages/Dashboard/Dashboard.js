d25db572e980395bdd73c3f5581beb64331ba130
Manoel <manoelluisbusiness@gmail.com>
2022-07-22 00:55:04 -0300

    Dashboard finalizada

--- 10_MINIBLOG/miniblog/src/pages/Dashboard/Dashboard.js
+++ 10_MINIBLOG/miniblog/src/pages/Dashboard/Dashboard.js
@@ -9,11 +9,16 @@ const Dashboard = () => {
   const { user } = useAuthValue();
   const uid = user.uid;
 
-  // posts do usuario
-  const posts = [];
+  const { documents: posts, loading } = useFetchDocuments("posts", null, uid);
+
+  const deleteDocument = (id) => {};
+
+  if (loading) {
+    return <p>Carregando...</p>;
+  }
 
   return (
-    <div>
+    <div className={styles.dashboard}>
       <h1>Dashboard</h1>
       <p>Gerecie seus posts</p>
       {posts && posts.length === 0 ? (
@@ -24,7 +29,36 @@ const Dashboard = () => {
           </Link>
         </div>
       ) : (
-        <div></div>
+        <>
+          <div className={styles.post_header}>
+            <span>Titulo</span>
+            <span>Ações</span>
+          </div>
+
+          {posts &&
+            posts.map((post) => (
+              <div key={post.id} className={styles.post_row}>
+                <p>{post.title}</p>
+                <div>
+                  <Link to={`/posts/${post.id}`} className="btn btn-outline">
+                    Ver
+                  </Link>
+                  <Link
+                    to={`/posts/edit/${post.id}`}
+                    className="btn btn-outline"
+                  >
+                    Editar
+                  </Link>
+                  <button
+                    onClick={() => deleteDocument(post.id)}
+                    className="btn btn-outline btn-danger"
+                  >
+                    Excluir
+                  </button>
+                </div>
+              </div>
+            ))}
+        </>
       )}
     </div>
   );
